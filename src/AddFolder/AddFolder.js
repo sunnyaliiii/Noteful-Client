3cbb02ece9247077c0a6987359217061b1c48fdc
sunnyAli <57465011+sunnyaliiii@users.noreply.github.com>
2020-04-25 14:48:39 -0700

    Update AddFolder.js

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,80 +1,89 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
-import './AddFolder.css';
-import AppContext from '../AppContext';
-import ValidationError from '../ValidationError/ValidationError';
-import NotefulApi from '../NotefulService';
+import Error from '../Error'
+import PropTypes from 'prop-types'
+import './AddFolder.css'
+import NoteContext from '../NoteContext'
+require('dotenv').config()
+
 
 export default class AddFolder extends Component {
-  static contextType = AppContext;
+  static contextType = NoteContext;
 
-  state = {
-    name: '', nameValid: false, validateMsg: '',
-    formValid: false,
-    error: null
-  }
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const folder = {name: this.state.name}
-    
-    try {
-      const newFolder = await new NotefulApi().addFolder(folder)
-      this.setState({error: null})
-      this.props.history.push('/');
-      this.context.addFolder(newFolder)
-    } catch(err) {
-      this.setState({error: err.message})
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      nameValid: false,
+      validationMessage: ''
     }
   }
 
-  updateName = (name) => {
-    this.setState({name}, () => this.validateName(name))
-  }
-
-  validateName = name => {
-    let nameValid = true;
-    let validateMsg;
-
-    if (name === '') {
-      nameValid = false;
-      validateMsg = 'Name cannot be blank'
-    }
-
-    this.setState({nameValid, validateMsg}, this.validateForm)
-  }
-
-  validateForm = () => {
+  handleNameChange = name => {
     this.setState({
-      formValid: this.state.nameValid
+      name
     })
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 5ea97ca2-02d9-4c1e-9089-8c26f957fdb8`
+      }, 
+      body: JSON.stringify({name: this.state.name})
+    }
+    fetch('http://localhost:8000/api/folders/', options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong. Please refresh the page.')
+        }
+        return res;
+      })
+      .then(data => data.json())
+      .then(results => {
+        const newFolder = {
+          name: results.name,
+          id: results.id
+        }
+        this.context.handleFolderAdd(newFolder)
+      })
+      .then(this.props.history.goBack())
+      .catch(err => console.error(err))
   }
 
   render() {
     return (
-      <section className='AddFolder'>
-        <h2>Create a folder</h2>
-        <div className='error-message' style={this.state.error ? {display: 'block'} : {display: 'none'} }>{this.state.error}</div>
-        <NotefulForm onSubmit={e => this.handleSubmit(e)}>
-          <div className='field'>
-            <label htmlFor='folder-name-input'>
-              Name
-              < ValidationError isValid={this.state.nameValid} message={this.state.validateMsg} />
-            </label>
-            <input 
-              type='text' 
-              id='folder-name-input'
-              defaultValue=''
-              onChange={e => this.updateName(e.target.value)}
-            />
-          </div>
-          <div className='buttons'>
-            <button type='submit' disabled={!this.state.formValid}>
-              Add folder
-            </button>
-          </div>
-        </NotefulForm>
-      </section>
+      <Error>
+        <section className='AddFolder'>
+          <h2>Create a folder</h2>
+          <NotefulForm onSubmit={this.handleFormSubmit}>
+            <div className='field'>
+              <label htmlFor='folder-name-input'>
+                Name
+              </label>
+              <input 
+                type='text' 
+                id='folder-name-input'
+                onChange={e => this.handleNameChange(e.target.value)} />
+            </div>
+            <div className='buttons'>
+              <button type='submit'>
+                Add folder
+              </button>
+            </div>
+          </NotefulForm>
+        </section>
+      </Error>
     )
   }
+}
+
+AddFolder.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func
+  })
 }
